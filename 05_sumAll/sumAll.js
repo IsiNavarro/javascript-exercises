1d903922b955df1814b26f04346cdf3d700691b1
Isidro Navarro Oporto <9091821+IsiNavarro@users.noreply.github.com>
2023-01-10 17:01:00 +0100

    Finish ex 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    if (typeof a == "number" &&
+        typeof b == "number" &&
+        a >=0 && b >= 0){ // I don't get why this is needed. Negative numbers are still integers...
+        let sum = 0;
+        if (b>a){
+            for (let i = a; i<=b; i++){
+                sum += i;
+            }
+        } else {
+            for (let i = b; i<=a; i++){
+                sum += i;
+            }
+        }
+        return sum;
+    } else  {
+        return "ERROR";
+    }
 
 };
 
