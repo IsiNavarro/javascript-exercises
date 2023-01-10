c49d5b89a4c86cee5455ded40997df147b7e0274
Isidro Navarro Oporto <9091821+IsiNavarro@users.noreply.github.com>
2023-01-10 16:15:13 +0100

    Finish ex 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let array = [];
 
+    for (let i=0; i<string.length; i++){
+        array.unshift(string.charAt(i));
+    }
+
+    let reversedString = array.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
