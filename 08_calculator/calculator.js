575c3635c6253e8a066f715259cd86fe405b723a
Isidro Navarro Oporto <9091821+IsiNavarro@users.noreply.github.com>
2023-01-25 23:23:35 +0100

    Finish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  array.forEach(num => {
+    sum += num;
+  })
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  array.forEach(num => {
+    product = product*num;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = 1;
+	for(let i = 1; i<=a; i++){
+    factorial = factorial * i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
