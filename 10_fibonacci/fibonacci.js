fb140ae9aee2c78b2d2d0ea5075d617b971a37df
Isidro Navarro Oporto <99091821+IsiNavarro@users.noreply.github.com>
2023-01-26 20:48:49 +0100

    Finish 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+    let fibonacciSequence = [1, 1];
+
+    if(number <= 0){
+        return "OOPS";
+    }
+    for(let i = 2; i<number; i++ ){
+        fibonacciSequence[i] = fibonacciSequence[i-1]+fibonacciSequence[i-2];
+    }
+
+    return fibonacciSequence[number-1];
 };
 
 // Do not edit below this line
