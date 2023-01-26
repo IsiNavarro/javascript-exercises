6f1e8f95e7bb8b7d873db992b2fbed1bbe73463f
Isidro Navarro Oporto <99091821+IsiNavarro@users.noreply.github.com>
2023-01-26 20:41:02 +0100

    Finish 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let newString = string.toLowerCase()
+    .replace(/[^\w\s\']|_/g, "") //found on StackOverflow: strips punctuation
+    .replace(/\s+/g, "");        //strips whitespaces
+
+    for(let i = 0; i < newString.length; i++){
+        if(newString.charAt(i) != newString.charAt(newString.length -1 -i)){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
