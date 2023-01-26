6b3e426818b16cc389cf5a8c73184bf0c39048df
Isidro Navarro Oporto <99091821+IsiNavarro@users.noreply.github.com>
2023-01-26 21:11:22 +0100

    Finish 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let today = new Date();
+    let year = today.getFullYear();
+    people.forEach(person => person["yearOfDeath"] === undefined ? person["yearOfDeath"] = year: person["yearOfDeath"] = person["yearOfDeath"]);
+    console.log(people[0]["yearOfDeath"]);
+    let sortedPeople = people.sort((a, b) =>(a["yearOfDeath"]-a["yearOfBirth"]) > (b["yearOfDeath"]-b["yearOfBirth"]) ? -1:1);
+    return sortedPeople[0];
 };
 
 // Do not edit below this line
